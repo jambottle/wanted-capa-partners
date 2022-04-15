4e08d8910cd5048f5d64ae49a9b560cc15c130b7
jambottle <jaewon.walks@gmail.com>
2022-04-15 14:10:15 +0900

    feat: MenuSlider 컴포넌트의 토글 효과를 위한 이벤트 핸들러 등록 - MenuSlider 컴포넌트의 노출 상태를 관리하기 위해 useState Hook 활용 - useState Hook의 상태 값과 함수를 MenuSlider 컴포넌트에 props로 전달 - 기타 (img 태그의 alt 텍스트 및 src 이미지 파일명 일부 수정 등)

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,21 +1,28 @@
+import { useState } from 'react';
+import MenuSlider from 'components/NavBar/MenuSlider';
 import * as S from './style';
 
 function NavBar() {
+  const [isSliderVisible, setIsSliderVisible] = useState(false);
+
   return (
     <S.Container>
       <S.MenuIcon
-        alt="Menu Icon"
+        alt="Icon for Opening Navigation Menu"
         src={process.env.PUBLIC_URL + 'assets/icons/icon_menu.png'}
+        onClick={() => setIsSliderVisible(true)}
       />
+      <MenuSlider isVisible={isSliderVisible} onClick={setIsSliderVisible} />
+
       <S.MenuLogo
-        alt="CAPA Partners Logo"
+        alt="Logo of CAPA Partners"
         src={process.env.PUBLIC_URL + 'assets/images/logo_white.png'}
       />
       <S.UserInfo>
         <img
           className="userIcon"
           alt="User Icon"
-          src={process.env.PUBLIC_URL + 'assets/icons/icon_user.png'}
+          src={process.env.PUBLIC_URL + 'assets/icons/picto_white.png'}
         />
         <span className="userName">A 가공 업체</span>
         <span className="divider" />
