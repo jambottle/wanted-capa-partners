7351335d4bea7da8685912431aed054b5a5a0168
jambottle <jaewon.walks@gmail.com>
2022-04-15 16:20:18 +0900

    feat: ReqList 컴포넌트 및 ReqItem 컴포넌트 레이아웃 구현 - 상위 컴포넌트인 ReqList에서 ReqItem(요청서 카드)를 List Rendering으로 반복 - Mock API Server를 구축하기에 앞서 Mock Data를 List Rendering에 임시 활용 - props로 전달하는 data의 타입 지정은 앞서 저장해둔 ItemType을 공통 적용

---
+++ src/components/ReqList/ReqItem/index.tsx
@@ -0,0 +1,53 @@
+import { ItemType } from 'types';
+import * as S from './style';
+
+interface Props {
+  item: ItemType;
+}
+
+function ReqItem({ item }: Props) {
+  const { title, client, due, count, amount, method, material, status } = item;
+
+  return (
+    <S.Wrapper>
+      {/* 요청서 개요 */}
+      <h3>{title}</h3>
+      <h5>{client}</h5>
+      <span className="due">{due}까지 납기</span>
+      <hr />
+
+      {/* 요청서 상세내역 */}
+      <dl>
+        <dt>도면개수</dt>
+        <dd>{count}개</dd>
+      </dl>
+      <dl>
+        <dt>총 수량</dt>
+        <dd>{amount}개</dd>
+      </dl>
+      <dl>
+        <dt>가공방식</dt>
+        <dd>{method.join(', ')}</dd>
+      </dl>
+      <dl>
+        <dt>재료</dt>
+        <dd>{material.join(', ')}</dd>
+      </dl>
+
+      {/* 요청서 진행 버튼 */}
+      <div>
+        <button type="button" className="show">
+          요청 내역 보기
+        </button>
+        <button type="button" className="chat">
+          채팅하기
+        </button>
+      </div>
+
+      {/* 요청서 진행 현황 */}
+      {status === '상담중' && <span className="status">{status}</span>}
+    </S.Wrapper>
+  );
+}
+
+export default ReqItem;
