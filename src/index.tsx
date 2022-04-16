678489736b099a20ac5d8ee2a9dd4fbfb40fd93c
jambottle <jaewon.walks@gmail.com>
2022-04-17 02:20:42 +0900

    fix: ReactDOM.render로 인한 오류 수정 (React 18에서 지원 중단)

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import GlobalStyle from 'styles/GlobalStyle';
 import GlobalFonts from 'styles/GlobalFonts';
 import Router from 'Router';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <GlobalStyle />
     <GlobalFonts />
     <Router />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
